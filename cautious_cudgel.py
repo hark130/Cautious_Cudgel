ac0ea3ef28e1e3df508fe5d5c39d26d75df4aa0c
hark130 <hark130@yahoo.com>
2018-04-20 12:30:22 -0500

    master: Created c_c.py framework
    cautious_cudgel.py framework now includes:
    	Cleaned up (?) code
    	curses framework
    	Two different methods to procure packets
    
    I commented out some old helper functions at the end of the file because I didn't want to lose that code.  I'm sure I'll remove them later.

--- cautious_cudgel.py
+++ cautious_cudgel.py
@@ -1,89 +1,52 @@
+import curses
 import pyshark
 
 
-def print_ip_details(packet):
+def curse_a_win():
     '''
-    PURPOSE - Print details from the IP header
-    INPUT - pyshark Packet object
-    OUTPUT - None
+    PURPOSE - Initialize a curses window
+    INPUT - None
+    OUTPUT - 
+        On success, an initialzied window object
+        On failure, None (or Exception)
+    NOTES
+        Any modifications to the window initialization need to be reflected
+            in break_a_curse()
     '''
-    ### INPUT VALIDATION ###
-    if not isinstance(packet, pyshark.packet.packet.Packet):
-        raise ValueError("Invalid packet type")
+    ### INIT A WINDOW ###
+    # 1. Initialize curses
+    stdscr = curses.initscr()
 
-    ### PRINT IPV4 ###
-    try:
-        print("Source IP:       {}".format(packet.ip.src))
-        print("Destination IP:  {}".format(packet.ip.dst))
-    except AttributeError:
-        # Ignore any packets missing the above
-        # print("Not an IP packet!")  # DEBUGGING
-        # print(packet.layers)  # DEBUGGING
-        # print("{}".format(dir(packet)))  # DEBUGGING
-        pass
-
-    ### PRINT IPV6 ###
-    try:
-        print("Source IP:       {}".format(packet.ipv6.src))
-        print("Destination IP:  {}".format(packet.ipv6.dst))
-    except AttributeError:
-        # Ignore any packets missing the above
-        pass
+    if stdscr:
+        ### CONFIGURE THE WINDOW ###
+        # 2. Turn off automatic echoing of keys to the screen
+        curses.noecho()
+        # 3. React to keys instantly
+        curses.cbreak()
+        # 4. Set non-blocking read
+        stdscr.timeout(0)
 
     ### DONE ###
-    return
+    return stdscr
 
 
-def print_tcp_details(packet):
+def break_a_curse(cWin):
     '''
-    PURPOSE - Print details from the TCP header
-    INPUT - pyshark Packet object
+    PURPOSE - Terminate a curses window and restore the terminal
+    INPUT
+        cWin - A curses window object
     OUTPUT - None
     '''
-    ### INPUT VALIDATION ###
-    if not isinstance(packet, pyshark.packet.packet.Packet):
-        raise ValueError("Invalid packet type")
+    ### LOCAL VARIABLES ###
+    retVal = True  # Make this False if anyting fails
 
-    ### PRINT ###
-    try:
-        print("Source Port:       {}".format(packet.tcp.srcport))
-        print("Destination Port:  {}".format(packet.tcp.dstport))
-    except AttributeError:
-        # Ignore any packets missing the above
-        # print("Not a TCP packet!")  # DEBUGGING
-        # print(packet.layers)  # DEBUGGING
-        pass
-
-    ### DONE ###
-    return
-
-
-def print_enip_details(packet):
-    '''
-    PURPOSE - Print details from an ENIP header
-    INPUT - pyshark Packet object
-    OUTPUT - None
-    '''
-    ### INPUT VALIDATION ###
-    if not isinstance(packet, pyshark.packet.packet.Packet):
-        raise ValueError("Invalid packet type")
-
-    ### PRINT ###
-    try:
-        print("\n")  # Clean up
-        print("ENIP Command:         {}".format(packet.enip.command))
-        print("ENIP Session Handle:  {}".format(packet.enip.session))        
-    except AttributeError:
-        # Ignore any packets missing the above
-        pass
-    else:
-        try:
-            print_ip_details(packet)
-            print_tcp_details(packet)
-        except AttributeError:
-            pass
-    finally:
-        print("\n")  # Clean up
+    ### TERMIANTE A WINDOW ###
+    # 1. Turn off cbreak
+    curses.nocbreak()
+    # 2. Restore echo
+    curses.noecho()
+    # 3. Restore the terminal
+    curses.endwin()
 
     ### DONE ###
     return
@@ -91,28 +54,120 @@ def print_enip_details(packet):
 
 if __name__ == "__main__":
     ### LOCAL VARIABLES ###
-    
+    stdscr = curse_a_win()  # Initialize a curses window
 
     ### WORK ###
-    # 1. START LIVE CAPTURE
-    # Consider using the following Wireshark display_filters in the LiveCapture() call:
-    #   display_filter="enip.session"
-    #   display_filter="enip.command"
-    # https://wiki.wireshark.org/DisplayFilters
-    # capture = pyshark.LiveCapture(interface='enp0s25')  # capture object returned by pyshark.LiveCapture()
-    # capture = pyshark.LiveCapture()  # capture object returned by pyshark.LiveCapture()
-    # for packet in capture.sniff_continuously():
-    #     print_ip_details(packet)
-    #     print_tcp_details(packet)
-    #     print_enip_details(packet)
-        # print("Got a packet: {}", packet)
-        # print(packet)
+    if stdscr:
+        # 1. GET PACKETS
+        # 1.1. Real input
+        # capture = pyshark.LiveCapture()
+        # for packet in capture.sniff_continuously():
+        # 1.1. Test input
+        filename = "pcaps/talabor1_1.pcap"
+        fcapture = pyshark.FileCapture(filename)
+        for packet in fcapture:
+            pass
 
-    # 2. READ FROM STATIC FILE
-    filename = "pcaps/talabor1_1.pcap"
-    fcapture = pyshark.FileCapture(filename)
+        # Time to end?
+        while -1 == stdscr.getch():
+            continue
 
-    for packet in fcapture:
-        print_enip_details(packet)
+        ### BREAK THE CURSE ###
+        break_a_curse(stdscr)
 
 
+
+
+
+
+
+
+
+
+### OLD FUNCTIONS ###
+# def print_ip_details(packet):
+#     '''
+#     PURPOSE - Print details from the IP header
+#     INPUT - pyshark Packet object
+#     OUTPUT - None
+#     '''
+#     ### INPUT VALIDATION ###
+#     if not isinstance(packet, pyshark.packet.packet.Packet):
+#         raise ValueError("Invalid packet type")
+
+#     ### PRINT IPV4 ###
+#     try:
+#         print("Source IP:       {}".format(packet.ip.src))
+#         print("Destination IP:  {}".format(packet.ip.dst))
+#     except AttributeError:
+#         # Ignore any packets missing the above
+#         # print("Not an IP packet!")  # DEBUGGING
+#         # print(packet.layers)  # DEBUGGING
+#         # print("{}".format(dir(packet)))  # DEBUGGING
+#         pass
+
+#     ### PRINT IPV6 ###
+#     try:
+#         print("Source IP:       {}".format(packet.ipv6.src))
+#         print("Destination IP:  {}".format(packet.ipv6.dst))
+#     except AttributeError:
+#         # Ignore any packets missing the above
+#         pass
+
+#     ### DONE ###
+#     return
+
+
+# def print_tcp_details(packet):
+#     '''
+#     PURPOSE - Print details from the TCP header
+#     INPUT - pyshark Packet object
+#     OUTPUT - None
+#     '''
+#     ### INPUT VALIDATION ###
+#     if not isinstance(packet, pyshark.packet.packet.Packet):
+#         raise ValueError("Invalid packet type")
+
+#     ### PRINT ###
+#     try:
+#         print("Source Port:       {}".format(packet.tcp.srcport))
+#         print("Destination Port:  {}".format(packet.tcp.dstport))
+#     except AttributeError:
+#         # Ignore any packets missing the above
+#         # print("Not a TCP packet!")  # DEBUGGING
+#         # print(packet.layers)  # DEBUGGING
+#         pass
+
+#     ### DONE ###
+#     return
+
+
+# def print_enip_details(packet):
+#     '''
+#     PURPOSE - Print details from an ENIP header
+#     INPUT - pyshark Packet object
+#     OUTPUT - None
+#     '''
+#     ### INPUT VALIDATION ###
+#     if not isinstance(packet, pyshark.packet.packet.Packet):
+#         raise ValueError("Invalid packet type")
+
+#     ### PRINT ###
+#     try:
+#         print("\n")  # Clean up
+#         print("ENIP Command:         {}".format(packet.enip.command))
+#         print("ENIP Session Handle:  {}".format(packet.enip.session))        
+#     except AttributeError:
+#         # Ignore any packets missing the above
+#         pass
+#     else:
+#         try:
+#             print_ip_details(packet)
+#             print_tcp_details(packet)
+#         except AttributeError:
+#             pass
+#     finally:
+#         print("\n")  # Clean up
+
+#     ### DONE ###
+#     return
